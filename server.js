a30f346c8b74d81061b125125d7aad5742921682
Parvaggarawal01 <parvaggarwal130@gmail.com>
2026-02-11 18:32:44 +0530

    added rate limiter

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ import app from "./app.js";
 import connectDB from "./config/db.js";
 import { startCronJobs } from "./cron/testing.js"
 
-const PORT = 3000; //process.env.PORT || 5001;
+const PORT = process.env.PORT || 3000;
 
 connectDB();
 
