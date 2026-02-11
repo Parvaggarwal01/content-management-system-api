9b183b296c099f7936a7805fcea7b882be084311
Parvaggarawal01 <parvaggarwal130@gmail.com>
2026-02-11 11:45:59 +0530

    Adding file using cloudinary

---
+++ config/cloudinary.js
@@ -0,0 +1,13 @@
+  import { v2 as cloudinary } from "cloudinary";
+
+  // Configuration
+  cloudinary.config({
+    cloud_name: process.env.CLOUD_NAME,
+    api_key: process.env.API_KEY,
+    api_secret: process.env.API_SECRET, // Click 'View API Keys' above to copy your API secret
+  });
+
+  console.log("KEYS", process.env.CLOUD_NAME,process.env.API_KEY,process.env.API_SECRET);
+
+
+  export default cloudinary;
--- middleware/uploads.middleware.js
+++ middleware/uploads.middleware.js
@@ -1,15 +1,12 @@
 import multer from "multer";
-import path from "path";
+import { CloudinaryStorage } from "multer-storage-cloudinary";
+import cloudinary from "../config/cloudinary.js";
 
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, "uploads/");
-  },
-
-  filename: (req, file, cb) => {
-    const uniqueName = Date.now() + "-" + Math.round(Math.random() * 1e9);
-
-    cb(null, uniqueName + path.extname(file.originalname));
+const storage = new CloudinaryStorage({
+  cloudinary: cloudinary,
+  params: {
+    folder: "cms-uploads",
+    allowed_formats: ["jpg", "png", "jpeg", "pdf"],
   },
 });
 
